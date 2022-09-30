c86dbe24f308f80d420e4918ff74b8322b3e70a9
nselvak <selvakumar9155@gmail.com>
2022-09-30 14:55:27 +0800

    final

--- order-backend/src/main/java/vttp2022/assessment/csf/orderbackend/controllers/OrderRestController.java
+++ order-backend/src/main/java/vttp2022/assessment/csf/orderbackend/controllers/OrderRestController.java
@@ -1,5 +1,61 @@
 package vttp2022.assessment.csf.orderbackend.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vttp2022.assessment.csf.orderbackend.models.Order;
+import vttp2022.assessment.csf.orderbackend.models.OrderSummary;
+import vttp2022.assessment.csf.orderbackend.services.OrderService;
+
+@RestController
+@RequestMapping(path = "api/order")
 public class OrderRestController {
+
+    @Autowired
+    private JdbcTemplate template;
+
+    @Autowired
+    private OrderService svc ;
+
+
+    @GetMapping(path = "{email}")
+    public ResponseEntity< List<OrderSummary>> getOrders(@PathVariable String email) {
+        List<OrderSummary> opt = svc.getOrdersByEmail(email);
+
+
+        // if (opt.isEmpty()) {
+		// 	BookResponse resp = new BookResponse();
+		// 	resp.setStatus(404);
+		// 	resp.setMessage("Book %s not found".formatted(bookId));
+		// 	return ResponseEntity.status(HttpStatus.NOT_FOUND)
+		// 			.body(resp.toJson().toString());
+		// }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(opt);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> postUpoad(@RequestBody Order payload) {
+
+        System.out.println(">>>>>> payload" + payload);
+
+        svc.createOrder(payload);
+
+        return ResponseEntity.ok(payload.toString());
+    }
 
 }
--- order-backend/src/main/java/vttp2022/assessment/csf/orderbackend/services/OrderService.java
+++ order-backend/src/main/java/vttp2022/assessment/csf/orderbackend/services/OrderService.java
@@ -1,9 +1,13 @@
 package vttp2022.assessment.csf.orderbackend.services;
 
+import java.sql.ResultSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Service;
 
 import vttp2022.assessment.csf.orderbackend.models.Order;
@@ -12,13 +16,30 @@ import vttp2022.assessment.csf.orderbackend.models.OrderSummary;
 @Service
 public class OrderService {
 
+	public static final String SQL_SELECT_ORDERS =
+		"select order_id from orders where email=?";
+
+	public static final String SQL_INSERT_ORDERS =
+		"insert into orders(name, email, pizza_size, thick_crust, sauce, toppings, comments) values (?, ?, ?, ?, ?, ?, ?)";
+
 	@Autowired
 	private PricingService priceSvc;
+	@Autowired
+    private JdbcTemplate template;
 
 	// POST /api/order
 	// Create a new order by inserting into orders table in pizzafactory database
 	// IMPORTANT: Do not change the method's signature
 	public void createOrder(Order order) {
+
+		try {
+            int updated = template.update(SQL_INSERT_ORDERS, order.getName(), order.getEmail(),
+			 order.getSize(), order.isThickCrust(), order.getSauce(), order.getToppings(), order.getComments());;
+            System.out.printf("updated: %d\n", updated);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+		
 
 	}
 
@@ -27,5 +48,25 @@ public class OrderService {
 	// IMPORTANT: Do not change the method's signature
 	public List<OrderSummary> getOrdersByEmail(String email) {
 		// Use priceSvc to calculate the total cost of an order
+
+		List<OrderSummary> orders = new LinkedList<>();
+        SqlRowSet rs = template.queryForRowSet(SQL_SELECT_ORDERS, email);
+
+        while (rs.next()) {
+
+			Order p = new Order();
+
+			OrderSummary os = new OrderSummary();
+			os.setOrderId(rs.getInt("order_id"));
+			os.setName(rs.getString("name"));
+			os.setEmail((rs.getString("email")));
+			os.setAmount((float) 0);
+
+            orders.add(os);            
+        }
+
+        return orders;
+
+
 	}
 }
